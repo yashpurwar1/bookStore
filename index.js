091313b0f03c1460bb4f7f215de46a02ded1590a
yashpurwar1 <yashpurwar.yashpurwar@gmail.com>
2021-10-26 15:46:18 +0530

    first commit with index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require("dotenv").config();
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(express.json())
+
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to BookStore application."});
+});
+
+
+
+//Configuring the database
+const dbConfig = require('./config/database.config.js');
+dbConfig.connection();
+
+// require('./app/routes/user.routes.js')(app);
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port : ${process.env.PORT}`);
+});
+
+module.exports = app;
