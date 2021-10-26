d9e8a4e32c469d07206db907dcb83f286a81ec2c
yashpurwar1 <yashpurwar.yashpurwar@gmail.com>
2021-10-26 15:53:57 +0530

    Feat: Database connection

---
+++ config/database.config.js
@@ -0,0 +1,25 @@
+/**
+ * @module:         config
+ * @file:           database.config.js
+ * @description:    Database connectivity
+ * @author:         Yash
+ */
+
+ const mongoose = require('mongoose');
+ require("dotenv").config();
+ // Database connection
+ class DBconnection{
+     connection = () => {
+     mongoose.connect(process.env.URL, {
+         useNewUrlParser: true
+      }).then(() => {
+          console.log("Successfully connected to the database");    
+      }).catch(err => {
+          console.log('Could not connect to the database. Exiting now...', err);
+          process.exit();
+      });
+     }
+ }
+ 
+ module.exports = new DBconnection();
+ 
